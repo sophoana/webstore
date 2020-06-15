5633c6fff51192f242a241a78329fa1ce512a69c
Sophoana <span@orangetheory.com>
2020-06-14 21:50:23 -0500

    Init webstore project

---
+++ src/main/java/com/kdamprai/webstore/WebstoreApplication.java
@@ -0,0 +1,13 @@
+package com.kdamprai.webstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebstoreApplication.class, args);
+    }
+
+}
